4001fbf64408e0e739dbfdd0040596a33f2ac4e4
Vulpem <davidher96@gmail.com>
2017-05-16 20:58:32 +0200

    Messing with scene around

---
+++ AR_Practica3/Assets/CollectibleMovement.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectibleMovement : MonoBehaviour {
+
+    Vector3 originalPos;
+    public float displacement = 1.0f;
+    public float rotation_speed = 40.0f;
+    Transform trs;
+
+	// Use this for initialization
+	void Start () {
+        trs = GetComponent<Transform>();
+        originalPos = trs.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        trs.Rotate(trs.up, rotation_speed * Time.deltaTime);
+        trs.position = new Vector3(trs.position.x, originalPos.y + Mathf.Sin(Time.time) * displacement, trs.position.z);	
+	}
+}
